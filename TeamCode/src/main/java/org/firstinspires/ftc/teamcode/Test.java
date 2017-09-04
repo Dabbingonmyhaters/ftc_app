61149e36cce2b0bfa8f19738b905719c714277c6
Cristian <crisrosca657@gmail.com>
2017-09-04 16:54:47 -0700

    BOI

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by cristian on 9/4/2017.
+ */
+
+public class Test extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+    }
+}
