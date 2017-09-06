3592dd886759ec2ba1ea576baea3f4de6c2ec7e9
Cristian <crisrosca657@gmail.com>
2017-09-06 15:50:58 -0700

    BOI FIRSt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Russia.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.modules.GamepadV2;
+
+/**
+ * Created by cristian on 9/4/2017.
+ */
+@TeleOp
+public class Russia extends OpMode {
+        private DcMotor Left, Right;
+
+        private GamepadV2 pad1 = new GamepadV2();
+
+        @Override
+        public void init() {
+            Left = hardwareMap.dcMotor.get("L");
+            Right = hardwareMap.dcMotor.get("R");
+
+
+            Left.setDirection(DcMotorSimple.Direction.REVERSE);
+            Right.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+
+        @Override
+        public void loop() {
+            pad1.update(gamepad1);
+
+            Left.setPower(pad1.left_stick_y_exponential(1));
+            Right.setPower(pad1.right_stick_y_exponential(1));
+
+        }
+    }
+
